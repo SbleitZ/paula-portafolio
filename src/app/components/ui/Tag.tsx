1be6e4314f94ecdfc2ca03bfa8592b976425187e
Sbleitz <zptrack24@gmail.com>
2024-03-30 22:36:18 -0300

    feat: stroke and mobile version

--- src/app/components/ui/Tag.tsx
+++ src/app/components/ui/Tag.tsx
@@ -3,9 +3,9 @@ interface TagProps {
 }
 export default function Tag({ text }: TagProps) {
   return (
-    <div className="bg-linear-hero p-0.5 h-fit rounded-full">
+    <div className="mx-2 bg-linear-hero p-1 h-fit rounded-full">
       <div className="px-4 py-2 bg-bg w-full h-full rounded-full">
-        <span className="font-bold bg-clip-text text-transparent bg-linear-amarillo">
+        <span className="font-bold font-dmSans te lg:text-2xl text-xs bg-clip-text text-transparent bg-linear-amarillo">
           {text}
         </span>
       </div>
