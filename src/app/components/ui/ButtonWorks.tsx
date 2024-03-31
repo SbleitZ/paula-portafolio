18d51d4f37ce9c09b6c0d7b0d41eeb27502e9fea
Sbleitz <zptrack24@gmail.com>
2024-03-31 02:00:25 -0300

    feat: buttonworks component for myworks

---
+++ src/app/components/ui/ButtonWorks.tsx
@@ -0,0 +1,14 @@
+import { cn } from "@/app/utils/cn";
+
+interface ButtonWorksProps{
+  className?:string;
+  src:string;
+  alt:string;
+}
+export default function ButtonWorks({className,src,alt}:ButtonWorksProps){
+  return (
+    <div className={cn("bg-button p-5 rounded-3xl flex items-center justify-center lg:grow",className)}>
+      <img src={src} alt={alt} />
+    </div>
+  );
+}
