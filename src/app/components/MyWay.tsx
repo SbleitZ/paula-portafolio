17947e9a63b7b1eff69950eb8f3bbd13812ca2e4
Sbleitz <zptrack24@gmail.com>
2024-03-30 22:35:44 -0300

    feat: mywar mobile version

--- src/app/components/MyWay.tsx
+++ src/app/components/MyWay.tsx
@@ -1,21 +1,22 @@
 export default function MyWay() {
   return (
-    <section className="py-10 px-10 flex items-center justify-center flex-col gap-20">
-      <p className="text-5xl font-spaceMono font-bold text-paragraph">
+    <section className="py-10 px-6 lg:px-10 flex lg:items-center justify-center flex-col gap-20">
+      <p className="font-spaceMono text-2xl lg:text-5xl font-bold text-paragraph">
         Mi camino en el diseño
       </p>
 
-      <div className="font-nanumPen flex items-center justify-center">
-        <div className=" bg-clip-text text-transparent bg-linear text-floatText max-w-sm">
+      <div className="px-2 font-nanumPen flex lg:items-center justify-center flex-col md:flex-row max-md:gap-24">
+        <div className="bg-clip-text text-transparent bg-linear text-mb-floatText md:text-floatText max-w-64 md:max-w-sm">
           Comencé la CARRERA EN GRáfiCA y ANIMACIón digital en EL AÑO 2021.
         </div>
-        <img src="MyWay.svg" alt="my way svg" />
-        <div className="ml-6 bg-clip-text text-transparent bg-linear text-floatText max-w-md">
+        <img className="md:block hidden" src="MyWay.svg" alt="my way svg" />
+        <img className="md:hidden absolute  xs:right-10 right-0" src="MyWayMobile.svg" alt="my way svg" />
+        <div className="md:ml-6 bg-clip-text text-transparent bg-linear text-mb-floatText md:text-floatText max-w-64 md:max-w-sm">
           En el camino conocí el Diseño UX/UI, el cuál me enamoró por completo.
           Entonces comencé a tomar cursos en instituciones y de forma
           autodidacta.
           <img
-            className="relative left-[85%] "
+            className="relative left-[85%] lg:block hidden"
             src="Heart_Face.png"
             alt="cara enamorada"
           />
