342b9ae565b811d1af54dfaccdc76615cb6af294
Sbleitz <zptrack24@gmail.com>
2024-03-30 22:36:00 -0300

    feat: skills mobile version

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -12,7 +12,7 @@ const Tags = [
 export default function Skills() {
   return (
     <section className="py-10 font-spaceMono flex items-center justify-center flex-col gap-20">
-      <p className="text-5xl font-bold text-paragraph">Skills</p>
+      <p className="text-2xl lg:text-5xl font-bold text-paragraph">Skills</p>
       <div className="flex justify-center items-center gap-8 flex-wrap max-w-3xl">
         {Tags.map((tag, i) => (
           <Tag key={i} text={tag} />
