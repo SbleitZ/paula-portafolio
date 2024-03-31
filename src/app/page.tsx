827ea1afe101e5437981e9d8fd2aa3fae57cfeeb
Sbleitz <zptrack24@gmail.com>
2024-03-31 15:09:23 -0300

    update: new sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import Hero from "./components/Hero";
 import MyEducation from "./components/MyEducation";
 import MyWay from "./components/MyWay";
 import MyWorks from "./components/MyWorks";
+import Opinions from "./components/Opinions";
+import SantanderWork from "./components/SantanderWork";
 import Skills from "./components/Skills";
 
 // text -balance
@@ -14,6 +16,9 @@ export default function Home() {
       <MyWay />
       <MyEducation />
       <MyWorks />
+      
+      <SantanderWork/>
+      <Opinions />
       <Footer />
     </div>
   );
