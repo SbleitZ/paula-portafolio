3ab4093e15c328de6c33937f168dcb77d91c3daa
Sbleitz <zptrack24@gmail.com>
2024-04-05 22:16:05 -0300

    feat: rotate img

--- src/app/components/ui/Avatar.tsx
+++ src/app/components/ui/Avatar.tsx
@@ -5,7 +5,7 @@ interface AvatarProps {
 export default function Avatar({ src, alt }: AvatarProps) {
   return (
     <div className="flex items-center justify-center rounded-full w-56 h-56 bg-linear p-2">
-      <img className="size-48 aspect-video" src={src} alt={alt} />
+      <img className="rotate-[7.57deg] size-48 aspect-video" src={src} alt={alt} />
     </div>
   );
 }
