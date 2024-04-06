fc511f1d0b7060ea6bfa5d21804d63ac8776a8f1
Sbleitz <zptrack24@gmail.com>
2024-04-05 23:27:43 -0300

    update: anchor targets

--- src/app/components/ui/ButtonWorks.tsx
+++ src/app/components/ui/ButtonWorks.tsx
@@ -1,14 +1,16 @@
 import { cn } from "@/app/utils/cn";
+import Link from "next/link";
 
 interface ButtonWorksProps{
   className?:string;
   src:string;
   alt:string;
+  href:string;
 }
-export default function ButtonWorks({className,src,alt}:ButtonWorksProps){
+export default function ButtonWorks({className,src,alt,href}:ButtonWorksProps){
   return (
-    <div className={cn("bg-button p-5 rounded-3xl flex items-center justify-center lg:grow",className)}>
+    <Link href={href} className={cn("bg-button p-5 rounded-3xl flex items-center justify-center lg:grow",className)} target="_blank">
       <img src={"svgs/"+src} alt={alt} />
-    </div>
+    </Link>
   );
 }
