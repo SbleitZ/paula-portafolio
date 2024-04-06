bb59a0cde32eefd7230898c719b6ae8936d5bc6d
Sbleitz <zptrack24@gmail.com>
2024-04-05 22:34:50 -0300

    feat: new section santanderworks mobile and desktop design

--- src/app/components/SantanderWork.tsx
+++ src/app/components/SantanderWork.tsx
@@ -1,19 +1,23 @@
 export default function SantanderWork() {
   return (
-    <div className="w-full px-8 py-20 lg:px-20">
+    <div className="w-full px-8 pt-20 lg:pb-40 lg:px-20 max-lg:mt-24">
       <img
-          className="absolute left-[20%] h-[60%]"
-          src="SantanderArrow.svg"
+          className="absolute left-[20%] hidden lg:block "
+          src="svgs/SantanderArrow.svg"
           alt="santander arrow svg"
         />
-      <section className="flex justify-between ">
-        <div className="font-nanumPen text-mb-floatText bg-linear-AMA-VIO bg-clip-text text-transparent lg:text-floatText max-w-lg">
+      <section className="flex flex-col lg:justify-between gap-16">
+        <div className="max-lg:mt-10 font-nanumPen text-mb-floatText bg-linear-AMA-VIO bg-clip-text text-transparent lg:text-floatText max-w-lg">
           Trabajé durante 3 meses en una startup realizando tareas en ux/ui.{" "}
           <br /><br /> Design system, creación de nuevos componentesy análisis
           heurísticos.
         </div>
-        
-        <div className="self-end font-nanumPen text-mb-floatText bg-linear-AMA-VIO bg-clip-text text-transparent lg:text-floatText max-w-md">
+        <img
+          className="absolute block lg:hidden"
+          src="svgs/mbSantanderArrow.svg"
+          alt="santander arrow svg"
+        />
+        <div className="max-lg:mt-28 self-end font-nanumPen text-mb-floatText bg-linear-AMA-VIO bg-clip-text text-transparent lg:text-floatText max-w-md">
           Disfruto ser pixel perfect. Me encanta buscar la armonía y coherencia
           visual.
         </div>
