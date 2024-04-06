edb072be857280b060dd1b4b02fb2e7c433d546c
Sbleitz <zptrack24@gmail.com>
2024-04-06 14:10:18 -0300

    sr-only

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import MyEducation from "./components/MyEducation";
+import MyProyects from "./components/MyProyects";
 import MyWay from "./components/MyWay";
 import MyWorks from "./components/MyWorks";
 import Opinions from "./components/Opinions";
@@ -11,12 +12,13 @@ import Skills from "./components/Skills";
 export default function Home() {
   return (
     <div className="size-full bg-bg">
+      <div className="sr-only">developed by Sbleit - Github : https://github.com/sbleitz</div>
       <Hero />
       <Skills />
       <MyWay />
       <MyEducation />
       <MyWorks />
-      
+      <MyProyects />
       <SantanderWork/>
       <Opinions />
       <Footer />
