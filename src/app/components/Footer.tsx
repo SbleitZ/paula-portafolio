c6efc8fbe7c6056bf1ec274b6b5cb174680b8793
Sbleitz <zptrack24@gmail.com>
2024-04-05 23:28:16 -0300

    update: new properties for footer object

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,44 +3,35 @@ import { FaBehanceSquare } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FiPaperclip } from "react-icons/fi";
 import Underline from "./ui/Underline";
+import { cn } from "../utils/cn";
+
 const contacts = [
   {
     icon: <FaLinkedin color="white" size={64} title="linkedin link" />,
-    html: (
-      <>
-        Podés ver mi portafolio en{" "}
-        <Link className="flex justify-center" href="#">
-          <Underline text="Linkedin" gradiant="bg-linear-verde" />
-        </Link>
-      </>
-    ),
+    gradiantClassName:"bg-linear-verde",
+    href:"https://www.linkedin.com/in/pauladanielagareis/",
+    startContent:"Podés ver mi portafolio en",
+    text:"Linkedin",
+    underline:true,
   },
   {
     icon: <FiPaperclip color="white" size={64} title="paper link" />,
-    html: (
-      <>
-        Descargar{" "}
-        <Link
-          className="bg-clip-text text-transparent bg-linear-verde"
-          href="#"
-        >
-          CV
-        </Link>
-      </>
-    ),
+    gradiantClassName:"bg-linear-verde",
+    href:"https://drive.google.com/file/d/1iI3Nyx4aUWgRM8LLDR5smohcQLWMG-SV/view?usp=drive_link",
+    startContent:"Descargar",
+    text:"CV",
+    underline:false,
   },
   {
     icon: <FaBehanceSquare color="white" title="behance link" size={64} />,
-    html: (
-      <>
-        Podés contactarme por{" "}
-        <Link className="flex justify-center" href="#">
-          <Underline text="Behance" gradiant="bg-linear-violet-azul" />
-        </Link>
-      </>
-    ),
+    gradiantClassName:"bg-linear-violet-azul",
+    href:"https://www.behance.net/paugareis",
+    startContent:"Podóes contactarme por",
+    text:"Behance",
+    underline:true,
   },
 ];
+
 export default function Footer() {
   return (
     <footer className="w-full font-dmSans px-8 pt-20 lg:px-20 flex flex-col items-center">
@@ -55,7 +46,7 @@ export default function Footer() {
           className="bg-clip-text text-transparent bg-linear-violet-azul"
           href="#"
         >
-          Paula
+          Paula Gareis
         </Link>{" "}
         y desarrollado por
         <Link
@@ -73,13 +64,46 @@ export default function Footer() {
 }
 interface ContactBoxProps {
   icon: React.ReactNode;
-  html: React.ReactNode;
+  gradiantClassName: string;
+  href: string;
+  startContent: string;
+  text: string;
+  underline: boolean;
 }
-function ContactBox({ icon, html }: ContactBoxProps) {
+function ContactBox({ icon, gradiantClassName,href,startContent,text,underline }: ContactBoxProps) {
+  const properties = {gradiantClassName,href,startContent,text,underline };
   return (
     <div className="flex flex-col p-10 lg:p-20 gap-2 text-paragraph font-bold justify-center items-center text-mb-paragraphText lg:text-paragraphText">
       {icon}
-      <p className="text-center max-w-md">{html}</p>
+      <p className="text-center max-w-md">
+        <GradiantLink {...properties} />
+      </p>
     </div>
   );
+}
+
+interface GradiantLinkProps{
+  startContent?:string;
+  endContent?:string;
+  gradiantClassName:string;
+  href:string;
+  text:string;
+  underline?:boolean;
+}
+
+function GradiantLink({startContent,endContent,gradiantClassName,href,text,underline}:GradiantLinkProps){
+  return (
+  <>
+    {startContent}{" "}
+    <Link
+      className={cn("bg-clip-text text-transparent",underline ? "":gradiantClassName,underline ? "flex justify-center":"")}
+      href={href}
+      target="_blank"
+    >
+      {
+        underline ? <Underline text={text} gradiant={gradiantClassName} />:<>{text}</>
+      }
+    </Link>
+    {endContent && ""}{" "}
+  </>);
 }
